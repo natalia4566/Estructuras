fb5403d43d79d7662f2e9425d300c33c2e01b768
natalia4566 <140755966+natalia4566@users.noreply.github.com>
2024-04-22 18:13:28 -0500

    Add files via upload

--- FirstApp.jsx
+++ FirstApp.jsx
@@ -1,48 +1,12 @@
-import './index.js'
-import PropTypes from 'prop-types';
-import { useState } from 'react';
+const title = 'First App'; 
 
-const title = 'First app'; 
-
-const FirstApp = ({value}) =>{
-
-    const [counter, setCounter] = useState (value)
-    
-    const handleAdd = () => {
-        setCounter (counter + 1)
-    }
-
-    const handleSubstract = () => {
-        setCounter (counter - 1)
-    }
-    
-    const handleReset = () => {
-        setCounter (value)
-    }
-    return(
+const FirstApp = () => {
+    return (
         <>
-         <h1>Counter</h1>
-         <span> {counter} </span>
-         <button onClick={ () => handleAdd()}> +1</button>
-         <button onClick={ () => handleSubstract()}> -1</button>
-         <button onClick={ () => handleReset()}> Reset</button>
-       </>   
+        <h1>{title}</h1>
+        <span>10</span>
+        </>
     )
-   
 }
 
-
-
-FirstApp.propTypes = {
-    title: PropTypes.string.isRequired, // Establece si un vaor es obligatorio o no.
-    sum: PropTypes.number.isRequired
-
-}
-
-FirstApp.defaultProps = {
-    title: 'No hay titulo',
-    sum: 300
-}
-
-
-export default FirstApp; 
+export default FirstApp
